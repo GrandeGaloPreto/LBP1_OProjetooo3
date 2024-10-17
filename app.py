c5446ef194a163b5c3cf9f9e4971647bcef77785
GrandeGaloPreto <borges.murilo@aluno.ifsp.edu.br>
2024-10-17 20:55:50 -0300

    Tudo

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
-from flask import Flask, Blueprint
-from controllers.controller import blueprintrota
+from flask import Flask, render_template, session, request, redirect, url_for
+from controllers import login
+from models import cookies
 
-app = Flask(__name__)
-app.register_blueprint(blueprintrota)
+app = Flask(__name__, static_folder='static')
+app.register_blueprint(login.c)
+app.secret_key = 'xD da silva'
 
-if __name__ == "__main__":
+@app.before_request
+def b4_request():
+    if request.path == '/login':
+        return
+    if not cookies.existe('nome'):
+        return redirect(url_for('login.login'))
+
+@app.after_request
+def a_request(response):
+    print("Depois da requisição")
+    return response
+
+@app.route('/')
+def index():
+    return render_template('index.html', nome=cookies.get('nome'))
+
+if __name__ == '__main__':
     app.run(debug=True)
--- models/model.py
+++
@@ -1,9 +0,0 @@
-class Pessoa:
-    def __init__(self, nome, salario):
-        self.salario = salario
-        self.nome = nome
-
-def aumento():
-    novosalario = Pessoa.salario + Pessoa.salario/100*15
-    return novosalario
-
